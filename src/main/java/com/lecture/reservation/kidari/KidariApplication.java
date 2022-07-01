0d53f6ac22322ea274ea9da48ba0685ce49abeea
yjlee <yjlee@dfocus.net>
2022-07-01 21:46:34 +0900

    first commit

---
+++ src/main/java/com/lecture/reservation/kidari/KidariApplication.java
@@ -0,0 +1,13 @@
+package com.lecture.reservation.kidari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidariApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lecture/reservation/kidari/KidariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lecture.reservation.kidari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
